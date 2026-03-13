305b20c5bdc5772bfdd9abde49e20f9a451e1a22
Christopher Harrison <christopher.harrison@tweag.io>
2026-03-13 23:03:47 +0000

    SclsUtil::generate wrapper

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -3,13 +3,47 @@
 mod hs2rs;
 mod rs2hs;
 
+use std::collections::HashMap;
 use std::ffi::OsString;
+use std::fmt::Display;
 use std::io::Result;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
 /// Default binary name (must be in `$PATH`)
 const SCLS_UTIL_BIN: &str = "scls-util";
+
+/// Namespaces supported by scls-util when generating arbitrary files.
+#[derive(Hash)]
+pub(crate) enum Namespace {
+    BlocksV0,
+    GovCommitteeV0,
+    GovConstitutionV0,
+    GovPParamsV0,
+    GovProposalsV0,
+    NonceV0,
+    SnapshotsV0,
+    UtxoV0,
+}
+
+impl Display for Namespace {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::BlocksV0 => "blocks/v0",
+                Self::GovCommitteeV0 => "gov/committee/v0",
+                Self::GovConstitutionV0 => "gov/constitution/v0",
+                Self::GovPParamsV0 => "gov/pparams/v0",
+                Self::GovProposalsV0 => "gov/proposals/v0",
+                Self::NonceV0 => "nonce/v0",
+                Self::SnapshotsV0 => "snapshots/v0",
+                Self::UtxoV0 => "utx0/v0",
+            }
+        )
+    }
+}
 
 /// Wrapper for scls-util functionality.
 pub(crate) struct SclsUtil {
@@ -54,6 +88,31 @@ impl SclsUtil {
             .args(args)
             .status()
             .is_ok_and(|s| s.success())
+    }
+
+    /// Generate a random SCLS file with a count of the given namespaces.
+    pub(crate) fn generate(
+        &self,
+        namespaces: HashMap<Namespace, usize>,
+    ) -> Result<tempfile::NamedTempFile> {
+        let scls = tempfile::Builder::new().suffix(".scls").tempfile()?;
+
+        let mut args: Vec<OsString> = vec!["debug".into(), "generate".into(), scls.path().into()];
+
+        let mut total = 0;
+        for (namespace, &count) in &namespaces {
+            total += count;
+            if count != 0 {
+                args.extend(["--namespace".into(), format!("{namespace}:{count}").into()]);
+            }
+        }
+
+        if total == 0 {
+            return Err(std::io::Error::other("no namespaces specified"));
+        }
+
+        self.command().args(args).status()?;
+        Ok(scls)
     }
 }
 
